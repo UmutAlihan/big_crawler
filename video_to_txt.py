9dda2a6dadd3f35ff9762230b5e7923f354e5536
kemalcan <kemalcanbora@gmail.com>
2019-12-28 23:37:05 +0300

    test #1

--- video_to_txt.py
+++ video_to_txt.py
@@ -7,40 +7,49 @@ import os
 import glob
 
 class VideoToText:
-    def __init__(self,yt_url,file_name):
-        self.file_name =file_name
-        YouTube(yt_url).streams.first().download()
-        self.yt = YouTube(yt_url)
-        self.yt_download_video_convert_to_mp3()
-        self.mp3_to_wav_mem()
-        self.wav_chunks()
+    def __init__(self):
+       pass
 
-    def yt_download_video_convert_to_mp3(self):
-        print("yt",self.file_name)
-        self.yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(filename=self.file_name)
+    def yt_download_video_convert_to_mp3(self,videourl):
+        yt = YouTube(videourl)
+        video_ext = yt.embed_url.split("/embed/")[1]
 
-        video = VideoFileClip(os.path.join(self.file_name+".mp4"))
-        video.audio.write_audiofile(os.path.join(self.file_name+".mp3"))
+        yt = yt.streams.filter(progressive=True, file_extension='mp4')\
+            .order_by('resolution')\
+            .desc()\
+            .first()
 
-    def mp3_to_wav_mem(self):
-        sound = AudioSegment.from_mp3(self.file_name+".mp3")
-        sound.export(self.file_name+".wav", format="wav")
+        if not os.path.exists("static"):
+            os.makedirs("static")
 
-    def wav_chunks(self):
-        myaudio = AudioSegment.from_file(self.file_name+".wav", "wav")
-        chunk_length_ms = 3000  # pydub calculates in millisec
-        chunks = make_chunks(myaudio, chunk_length_ms)  # Make chunks of one sec
+        if not os.path.exists("static/"+video_ext):
+            os.makedirs("static/"+video_ext)
+
+        yt.download("static/"+video_ext,filename=video_ext)
+
+        video = VideoFileClip(os.path.join("static/"+video_ext+"/"+video_ext+".mp4"))
+        video.audio.write_audiofile(os.path.join("static/"+video_ext+"/"+video_ext+".mp3"))
+        sound = AudioSegment.from_mp3("static/"+video_ext+"/"+video_ext+".mp3")
+        sound.export("static/"+video_ext+"/"+video_ext+ ".wav", format="wav")
+
+        myaudio = AudioSegment.from_file("static/"+video_ext+"/"+video_ext+ ".wav", "wav")
+        chunk_length_ms = 3000  #
+        chunks = make_chunks(myaudio, chunk_length_ms)
 
         for i, chunk in enumerate(chunks):
-            chunk_name = "{}{}.wav".format(self.file_name,i)
+            chunk_name = "{}{}.wav".format(video_ext, i)
             print("exporting", chunk_name)
-            chunk.export("static/"+chunk_name, format="wav")
+            if not os.path.exists("static/" + video_ext+"_chunk"):
+                os.makedirs("static/" + video_ext+"_chunk")
+
+            chunk.export("static/"+ video_ext+"_chunk/"+chunk_name, format="wav")
 
 
-    def run(self):
-        tifCounter = len(glob.glob1("static", "*.wav"))
+    def run(self,video_ext):
+        tifCounter = len(glob.glob1("static/"+ video_ext+"_chunk/", "*.wav"))
         for i in range(tifCounter):
-            file = "{}{}.wav".format("static/{}".format(self.file_name),i)
+            file = "{}{}.wav".format("static/"+ video_ext+"_chunk/{}".format(video_ext),i)
+
             r = sr.Recognizer()
 
             with sr.AudioFile(file) as source:
@@ -52,4 +61,5 @@ class VideoToText:
                     print("Google Speech Recognition could not understand audio")
 
 
-VideoToText(yt_url="https://www.youtube.com/watch?v=vfZQK1G2O90",file_name="paramore").run()
+videourl = "https://www.youtube.com/watch?v=vfZQK1G2O90"
+VideoToText().run("vfZQK1G2O90")
